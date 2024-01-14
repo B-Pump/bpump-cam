0748b94d762047a6dc72a97e02d187955a4ef8d9
wiizz <wiizz76.contact@gmail.com>
2024-01-14 21:04:59 +0100

    ajouter une option pour lancer chaque exo

--- test.py
+++ test.py
@@ -5,10 +5,21 @@ exercices = Exercices()
 response = str(input(f"""{"="*100}
     Quelles exercice veux-tu faire ?
         1 - Pullup
+        2 - curl
+        3 - pushup
+        4 - situp
+        5 - squat
 {"="*100}
 
     Réponse: """))
 
 if response == "1":
-    print(exercices.start("squat", 3))
-    print(exercices.start("pullup", 2))
+    exercices.start("pullup", 3)
+elif response == "2": 
+    exercices.start("curl", 3)
+elif response == "3":
+    exercices.start("pushup", 3)
+elif response == "4":
+    exercices.start("situp", 3)
+elif response == "5":
+    exercices.start("squat", 3)
